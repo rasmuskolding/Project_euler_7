87e1efe8c65def35a4ab8582e62f8c863fb5135c
rasmuskolding <kolding.rasmus@gmail.com>
2017-03-05 00:00:45 +0100

    added problem in the bottom

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	N := 10001
 	n := 1
-	for x := 0; n < N; x++ { //x is the first number we check to see if it is prime)
+	for x := 0; n < N; x++ {
 		y := x / 2
 		for i := 2; i <= y; i++ {
 
@@ -19,7 +19,11 @@ func main() {
 					fmt.Println(x)
 				}
 			}
-
 		}
 	}
 }
+
+//Projekt Euler problem no. 7:
+//By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+//we can see that the 6th prime is 13.
+//What is the 10 001st prime number?
